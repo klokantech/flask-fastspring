2f1ffa4aa67f9dde4c1ec20d308cbc29ee502126
Václav Klusák <vaclav.klusak@klokantech.com>
2017-01-11 11:01:06 +0100

    Release 1.4.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-FastSpring',
-    version='1.3',
+    version='1.4',
     description='FastSpring API integration for Flask',
     py_modules=['flask_fastspring'],
     install_requires=[
