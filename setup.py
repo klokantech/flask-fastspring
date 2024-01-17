8684aabc276ef49d4dc560c0eade4b4cdae082eb
Jevhen Olehovyč Ponomarenko <jevhen.ponomarenko@maptiler.com>
2024-01-17 18:57:13 +0100

    Fix package version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-FastSpring',
-    version='1.17',
+    version='1.18',
     description='FastSpring API integration for Flask',
     py_modules=['flask_fastspring'],
     install_requires=[
